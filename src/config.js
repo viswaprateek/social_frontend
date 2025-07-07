7790af47a7137e9aad6930ef0a3ffc01fcfd87a9
viswaprateek <viswaprateek248@gmail.com>
2025-07-07 16:23:23 +0530

    config changed

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let BASE_URL = "https://post-it-heroku.herokuapp.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:4000/";
+  BASE_URL = "https://social-backend-y2zq.onrender.com/";
 }
 
 export { BASE_URL };
